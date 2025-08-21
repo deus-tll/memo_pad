6dfed69a338907dd77ce18edd81c962ccca2a05e
Artem Miller <millerartem.work@gmail.com>
2025-08-21 23:08:47 +0300

    feat: Set up frontend with Vite, React, Tailwind, and daisyUI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import DetailsPage from "./pages/DetailsPage";
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<DetailsPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/DetailsPage.jsx
@@ -0,0 +1,5 @@
+const DetailsPage = () => {
+  return <div>Note Page</div>;
+};
+
+export default DetailsPage;
