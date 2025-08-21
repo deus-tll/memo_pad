3f1ed625980006cec96cc3c4d4fdfe09f6b2397e
Artem Miller <millerartem.work@gmail.com>
2025-08-21 17:47:29 +0300

    feat: Add router for notes

---
+++ backend/src/routers/notes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { 
+    createNote,
+    deleteNote, 
+    getNote, 
+    getNotes, 
+    updateNote 
+} from "../controllers/notes.js";
+
+const router = express.Router();
+
+router.get("/", getNotes);
+router.get("/:id", getNote);
+router.post("/", createNote);
+router.put("/:id", updateNote);
+router.delete("/:id", deleteNote);
+
+export default router;
