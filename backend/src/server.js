5807fec0c9928a6df0747364cf912eab958e83c0
Artem Miller <millerartem.work@gmail.com>
2025-08-22 21:58:41 +0300

    feat: Add cors middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 import connectDB from "./config/db.js";
 import notesRouter from "./routers/notes.js"
@@ -11,6 +12,11 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(rateLimiter);
