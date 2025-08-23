8bfd63c7540460bed0bda8e5167dbde2d43bfa17
Artem Miller <millerartem.work@gmail.com>
2025-08-23 22:20:41 +0300

    feat: Prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path";
 
 import connectDB from "./config/db.js";
 import notesRouter from "./routers/notes.js"
@@ -8,15 +9,18 @@ import rateLimiter from "./middleware/rateLimiter.js";
 import apiErrorHandler from "./middleware/apiErrorHandler.js";
 
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
 const app = express();
 
 // Middleware
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(rateLimiter);
@@ -24,6 +28,14 @@ app.use(apiErrorHandler);
 
 // Routes
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 // Connection to the database and server launch
 connectDB().then(() => {
