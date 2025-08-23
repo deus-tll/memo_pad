f7beb7f8041b7bac35c9d318868ffbd450f4e859
Artem Miller <millerartem.work@gmail.com>
2025-08-23 18:56:17 +0300

    feat: Add condition of when to show component NotesNotFound.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import toast from "react-hot-toast";
 import Navbar from "../components/Navbar";
 import NoteCard from "../components/NoteCard.jsx";
 import RateLimitedUi from "../components/RateLimitedUI.jsx";
+import NotesNotFound from "../components/NotesNotFound.jsx";
+
 import api from "../lib/axios.js";
 
 const HomePage = () => {
@@ -44,10 +46,12 @@ const HomePage = () => {
 		<div className="max-w-7xl mx-auto p-4 mt-6">
 			{loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
+			{notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
 			{notes.length > 0 && !isRateLimited && (
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 					{notes.map(note => (
-						<NoteCard key={note._id} note={note} />
+						<NoteCard key={note._id} note={note} setNotes={setNotes} />
 					))}
 				</div>
 			)}
