8adb6652e92d5082fc5974e998c64fb5f009e958
Artem Miller <millerartem.work@gmail.com>
2025-08-23 18:51:58 +0300

    fix: Fix placing of imports in CreatePage.jsx

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import {Link, useNavigate} from "react-router";
 import {ArrowLeftIcon} from "lucide-react";
 import toast from "react-hot-toast";
+
 import api from "../lib/axios.js";
 
 const CreatePage = () => {
